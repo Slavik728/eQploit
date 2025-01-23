44fe1e41b4fc8147aaee1a0cf31d93ab7aa15839
Slavik <160470809+Slavik728@users.noreply.github.com>
2025-01-23 04:21:04 -0300

    tightened the height between the player and destroy height

--- hub/ScriptHub.lua
+++ hub/ScriptHub.lua
@@ -80,7 +80,7 @@ local ABtoggle = ToolTab:CreateToggle({
       if ABBool then
          local destroyHeight = game.Workspace.FallenPartsDestroyHeight
          originalPos = LocalRoot.CFrame
-         LocalRoot.CFrame = CFrame.new(0, destroyHeight+1.5, 0) * CFrame.fromEulerAngles(math.rad(90), 0, 0)
+         LocalRoot.CFrame = CFrame.new(0, destroyHeight+1, 0) * CFrame.fromEulerAngles(math.rad(90), 0, 0)
          task.wait(0.05) LocalRoot.Anchored = true
       else
          if originalPos ~= nil then
